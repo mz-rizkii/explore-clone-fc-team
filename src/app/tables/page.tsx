94a3d0aa994bf3382ebd2970929e60ff9f9a2238
mz-rizkii <van53belaz@gmail.com>
2024-04-30 11:30:56 +0700

    feat: add view for tables page

--- src/app/tables/page.tsx
+++ src/app/tables/page.tsx
@@ -1,3 +1,16 @@
+import { TableView } from "../components/tables/TableView";
+
 export default function TablePage() {
-  return <h2>Table</h2>
+  const teams = [
+    { position: 1, team_logo: '', team_name: 'Arsenal', played: 35, win: 25, draw: 5, lose: 5, goal_fielded: 85, goal_allowed: 28, goal_diff: 57, points: 80 },
+    { position: 2, team_logo: '', team_name: 'Manchester City', played: 34, win: 24, draw: 7, lose: 3, goal_fielded: 82, goal_allowed: 32, goal_diff: 50, points: 79 },
+    { position: 3, team_logo: '', team_name: 'Liverpool', played: 35, win: 22, draw: 9, lose: 4, goal_fielded: 77, goal_allowed: 36, goal_diff: 41, points: 75 },
+    { position: 4, team_logo: '', team_name: 'Aston Villa', played: 35, win: 20, draw: 7, lose: 8, goal_fielded: 73, goal_allowed: 52, goal_diff: 21, points: 67 },
+    { position: 5, team_logo: '', team_name: 'Tottenham Hotspur', played: 33, win: 18, draw: 6, lose: 9, goal_fielded: 67, goal_allowed: 52, goal_diff: 15, points: 60 },
+    { position: 6, team_logo: '', team_name: 'Manchester United', played: 34, win: 16, draw: 6, lose: 12, goal_fielded: 52, goal_allowed: 51, goal_diff: 1, points: 54 },
+  ];
+
+  const title = 'Premier League 2023-2024'
+
+  return <TableView title={title} teams={teams} />
 }
--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -9,3 +9,17 @@ export type PlayerStats = {
   name: string;
   stats: MiniStats[]
 } 
+
+export type StandingTable = {
+  position: number;
+  team_name: string;
+  team_logo: string;
+  played: number;
+  win: number;
+  draw: number;
+  lose: number;
+  goal_fielded: number;
+  goal_allowed: number;
+  goal_diff: number;
+  points: number;
+} 
